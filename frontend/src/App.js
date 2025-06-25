b05c1834c12b6b87f8cd6a6805797950e66f891f
thetnaing754 <usathetnaing@gmail.com>
2025-06-25 14:35:54 +0630

    port change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   const fetchMessage = async () => {
     try {
-      const response = await fetch('http://localhost:5000/api/message');
+      const response = await fetch('http://localhost:5001/api/message');
       const data = await response.json();
       setMessage(data.message);
     } catch (error) {
